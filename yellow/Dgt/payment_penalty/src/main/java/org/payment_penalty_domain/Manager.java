cce68640f5d5aa2da41a17e25e13f2261667c8a6
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2016-12-07 18:53:00 +0100

    Iteration 3. Version 3.0.0

---
+++ yellow/Dgt/payment_penalty/src/main/java/org/payment_penalty_domain/Manager.java
@@ -0,0 +1,28 @@
+package org.payment_penalty_domain;
+
+
+
+
+
+public class Manager {
+
+	private Manager() {
+		throw new UnsupportedOperationException();
+	}
+
+
+	public static Manager get() {
+		throw new UnsupportedOperationException();
+	}
+
+	
+	
+	public void paymentPenalty(int PenaltyId) {
+		throw new UnsupportedOperationException();
+	}
+
+
+	
+	
+	
+}
