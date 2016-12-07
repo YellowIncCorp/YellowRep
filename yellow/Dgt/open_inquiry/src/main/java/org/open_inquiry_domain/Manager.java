15d0f9f26df3adf2ed849b101ff96e2c7840592f
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2016-12-07 18:53:31 +0100

    Iteration 3. Version 3.0.0

--- yellow/Dgt/open_inquiry/src/main/java/org/open_inquiry_domain/Manager.java
+++ yellow/Dgt/open_inquiry/src/main/java/org/open_inquiry_domain/Manager.java
@@ -1,28 +1,16 @@
 package org.open_inquiry_domain;
 
 
-
-/**
- * This class is the access point to the business logic.
- * @author macario.polo
- */
-
 public class Manager {
 
 	private Manager() {
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * @return The only existing instance of the Manager
-	 */
 	public static Manager get() {
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * Executed by {@link Manager} when a vehicle passes through the radar exceeding the speed limit
-	 */
 	public int openInquiry(String aLicense, double aSpeed, String aLocation, double aMaxSpeed) {
 		throw new UnsupportedOperationException();
 	}
@@ -32,11 +20,7 @@ public class Manager {
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * Executed from the user interface when a vehicle changes its owner
-	 * @param aLicense The license number of the vehicle
-	 * @param aNewDni The dni of the new owner
-	 */
+
 	public void changeOwner(String aLicense, String aNewDni) {
 		throw new UnsupportedOperationException();
 	}
